419268979cab577f696a8957c8a3d8f1ed5fd099
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-05-27 16:07:26 -0300

    refactor(Filme): add launch year

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -20,7 +20,7 @@ public class Filme extends Titulo implements Classificavel {
 
     @Override
     public String toString() {
-        return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
+        return "Filme: " + this.getNome() + " (Ano de Lançamento: " + this.getAnoDeLancamento() + ")";
     }
 
     public Filme(String nome, int anoDeLancamento) {
