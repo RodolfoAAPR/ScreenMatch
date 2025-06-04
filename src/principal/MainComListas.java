a05606b2c4c61ec1ff2081f2450c97ee9afd8995
RodolfoAAPR <rodolfoaapr.tech@gmail.com>
2025-06-04 17:22:43 -0300

    feat(GerenciadorDeTitulos): add class to manage titles and artists with lists

--- src/principal/MainComListas.java
+++ src/principal/MainComListas.java
@@ -2,12 +2,7 @@ package principal;
 
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
-import br.com.alura.screenmatch.modelos.Titulo;
-
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import br.com.alura.screenmatch.servicos.GerenciadorDeTitulos;
 
 public class MainComListas {
     public static void main(String[] args) {
@@ -22,32 +17,21 @@ public class MainComListas {
 
         Serie serie = new Serie("Supernatural", 2017);
 
-        List<Titulo> lista = new LinkedList<>();
-        lista.add(meuFilme);
-        lista.add(meuFilme1);
-        lista.add(filmeDoPaulo);
-        lista.add(serie);
+        GerenciadorDeTitulos gerenciadorDeTitulos = new GerenciadorDeTitulos();
+        gerenciadorDeTitulos.adicionaTitulo(meuFilme);
+        gerenciadorDeTitulos.adicionaTitulo(meuFilme1);
+        gerenciadorDeTitulos.adicionaTitulo(filmeDoPaulo);
+        gerenciadorDeTitulos.adicionaTitulo(serie);
 
-        for(Titulo titulo : lista) {
-            System.out.println("Nome: " + titulo.getNome());
-            if (titulo instanceof Filme filme && filme.getClassificacao() > 2) {
-                System.out.println("Classificação: " + filme.getClassificacao());
-            }
-        }
+        gerenciadorDeTitulos.exibeNomeClassificacao();
 
-        List<String> buscaPorArtista= new LinkedList<>();
-        buscaPorArtista.add("Misha Collins");
-        buscaPorArtista.add("Jensen Ackles");
-        buscaPorArtista.add("Jared Padalecki");
-        System.out.println("\nArtistas Antes da Ordenação: " + buscaPorArtista);
+        gerenciadorDeTitulos.adicionaArtistas("Misha Collins");
+        gerenciadorDeTitulos.adicionaArtistas("Jensen Ackles");
+        gerenciadorDeTitulos.adicionaArtistas("Jared Padalecki");
+        gerenciadorDeTitulos.exibeArtistasDesordenados();
 
-        Collections.sort(buscaPorArtista);
-        System.out.println("Depois da ordenação: " + buscaPorArtista);
-
-        Collections.sort(lista);
-        System.out.println("\nLista de Títulos Ordenados: " + lista);
-
-        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
-        System.out.println("Filmes Ordenados por Ano: " + lista);
+        gerenciadorDeTitulos.exibeArtistasOrdenados();
+        gerenciadorDeTitulos.exibeTitulosOrdenados();
+        gerenciadorDeTitulos.exibeTitulosOrdenadosPorAno();
     }
 }
